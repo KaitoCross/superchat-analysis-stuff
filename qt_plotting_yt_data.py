0c480406855e37710acdfc71ef58170b714367e1
KaitoCross <christian_hund@hotmail.de>
2021-09-10 16:13:57 +0200

    Added graphs to analys the amount of users per hour a day that superchat. Also, stream schedue graph bugfixes

--- qt_plotting_yt_data.py
+++ qt_plotting_yt_data.py
@@ -25,6 +25,12 @@ class MyApp(QMainWindow, ui_design.Ui_MainWindow):
         self.getSCbutton.clicked.connect(self.get_superchats)
         self.startDateTimeEditor.setDisplayFormat("dd.MM.yyyy hh:mm")
         self.endDateTimeEditor.setDisplayFormat("dd.MM.yyyy hh:mm")
+        self.curr = {"Africa": ["ZAR","EGP"],
+                "Asia": ["AED","HKD","INR","JOD","JPY","KRW","MYR","PHP","QAR","SAR","SGD","TWD"],
+                "Europe": ["BAM","BGN","BYN","CHF","CZK","DKK","EUR","GBP","HRK","HUF","ILS","ISK","NOK","PLN","RON","RSD","RUB","SEK","TRY"],
+                "North America": ["USD","CAD","MXN"],
+                "South America": ["ARS","BOB","BRL","CLP","COP","CRC","DOP","GTQ","HNL","NIO","PEN","PYG","UYU"],
+                "Oceania": ["AUD","NZD"]}
         self.pgsql_config_file = open("postgres-config-qt.json")
         self.pgsql_creds = json.load(self.pgsql_config_file)
         self.sc_model = MySqlModel()
@@ -85,10 +91,14 @@ class MyApp(QMainWindow, ui_design.Ui_MainWindow):
                 endtime = stream[0] + stream[1]
                 while testtime <= endtime:
                     heatmap_data[testtime.weekday()][testtime.time().hour] += 1
+                    #if testtime.weekday() >= 0 and testtime.weekday() <= 4:
+                    #    heatmap_data[7][testtime.time().hour] += 1
+                    #elif testtime.weekday() >= 5:
+                    #    heatmap_data[8][testtime.time().hour] += 1
                     testtime = testtime + timedelta(hours=1)
             namecount += 1
         print(heatmap_data)
-        days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+        days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']#,'Weekdays','Weekend']
         self.plotWidget_2.canvas.ax.yaxis.set_major_locator(loc)
         self.plotWidget_2.canvas.ax.legend(loc="upper center", bbox_to_anchor=(0.5, 1.15), ncol=3, handles=patches)
         self.plotWidget_2.canvas.draw()
@@ -104,6 +114,26 @@ class MyApp(QMainWindow, ui_design.Ui_MainWindow):
             for j in range(24):
                 text = self.plotWidget_2.canvas.ax.text(j, i, heatmap_data[i, j],ha="center", va="center", color="w")
         self.heatmap_widget.canvas.draw()
+        #xFmt = mdates.DateFormatter('%H:%M')
+        self.donortiming.canvas.ax.clear()
+        self.donortiming.canvas.ax.set_title("Donors per currency at which time")
+        #self.donortiming.canvas.ax.xaxis.set_major_formatter(xFmt)
+        self.donortiming.canvas.ax.set_ylabel("amount of unique superchatters")
+        self.donortiming.canvas.ax.set_xlabel("Time of day")
+        time_dict, coord_dict, area_sums = self.get_supa_time(startTime,endTime)
+        for currency in coord_dict:
+            self.donortiming.canvas.ax.plot('time','users',data=coord_dict[currency],label = currency, color = self.choose_color(currency))
+        self.donortiming.canvas.ax.legend(loc="right")
+        self.donortiming.canvas.draw()
+        self.area_sc_timing_draw.canvas.ax.clear()
+        self.area_sc_timing_draw.canvas.ax.set_title("Donors per region at which time")
+        self.area_sc_timing_draw.canvas.ax.set_ylabel("amount of unique superchatters")
+        self.area_sc_timing_draw.canvas.ax.set_xlabel("Time of day")
+        print(area_sums)
+        for area in area_sums.keys():
+            self.area_sc_timing_draw.canvas.ax.plot(area_sums[area], label = str(area), color = self.choose_color(area))
+        self.area_sc_timing_draw.canvas.ax.legend(loc="right")
+        self.area_sc_timing_draw.canvas.draw()
         
     def populate_widgets(self):
         self.db = QSqlDatabase.addDatabase('QPSQL')
@@ -123,6 +153,11 @@ class MyApp(QMainWindow, ui_design.Ui_MainWindow):
         while query.next():
             #print(query.value(0))
             entry = QListWidgetItem(query.value(0).strip())
+            entry.setCheckState(Qt.Checked)
+            self.currencyListWidget.addItem(entry)
+            
+        for area in self.curr.keys():
+            entry = QListWidgetItem(area)
             entry.setCheckState(Qt.Checked)
             self.currencyListWidget.addItem(entry)
         self.db.close()
@@ -176,7 +211,7 @@ class MyApp(QMainWindow, ui_design.Ui_MainWindow):
         #    names = names[:-1] + ")"
         self.db.open()
         query = QSqlQuery()
-        query.prepare("select actualstarttime AT TIME ZONE 'UTC', length, scheduledStartTime  AT TIME ZONE 'UTC', endedLogAt AT TIME ZONE 'UTC' from video inner join channel c on c.id = channel_id where c.name = :name and ((actualstarttime >= :start and actualstarttime < :end) or (scheduledStartTime >= :start and scheduledStartTime < :end))")
+        query.prepare("select actualstarttime AT TIME ZONE 'UTC', length, scheduledStartTime AT TIME ZONE 'UTC', endedLogAt AT TIME ZONE 'UTC', actualendtime AT TIME ZONE 'UTC' from video inner join channel c on c.id = channel_id where c.name = :name and ((actualstarttime >= :start and actualstarttime < :end) or (scheduledStartTime >= :start and scheduledStartTime < :end))")
         query.bindValue(":name",chan_name)
         query.bindValue(":start",startTime)
         query.bindValue(":end",endTime)
@@ -190,13 +225,23 @@ class MyApp(QMainWindow, ui_design.Ui_MainWindow):
             plannedstarttime = query.value(2).toPyDateTime()
             endedLogAt = query.value(3)
             endedLogAt = endedLogAt.toPyDateTime() if endedLogAt else 0
+            actualEndTime = query.value(4)
+            actualEndTime = actualEndTime.toPyDateTime() if actualEndTime else 0
             if not starttime:
                 starttime = plannedstarttime
             #print(starttime,duration,endedLogAt)
             if not duration:
+                end_time = None
                 if not endedLogAt:
-                    endedLogAt = datetime.utcnow()
-                duration = endedLogAt - starttime
+                    now = datetime.utcnow()
+                    tempend = starttime + timedelta(hours = 3)
+                    endedLogAt = now if (now - starttime) < timedelta(hours = 12) else tempend
+                    end_time = endedLogAt
+                else:
+                    end_time = endedLogAt
+                if actualEndTime:
+                    end_time = actualEndTime
+                duration = end_time - starttime
                 #print("#",starttime,duration)
             if starttime and duration:
                 stream_list.append((starttime,duration))
@@ -205,17 +250,9 @@ class MyApp(QMainWindow, ui_design.Ui_MainWindow):
         
     def get_time_series_data(self,startTime,endTime):
         #print(startTime.toString(), endTime.toString())
-        checked_chans = []
-        for index in range(self.channelListWidget.count()):
-            if self.channelListWidget.item(index).checkState() == Qt.Checked:
-                checked_chans.append(self.channelListWidget.item(index))
-        namelist = [x.text() for x in checked_chans]
+        namelist = self.get_checked_chans()
         names = self.namelist_string(namelist)
-        checked_currencies = []
-        for index in range(self.currencyListWidget.count()):
-            if self.currencyListWidget.item(index).checkState() == Qt.Checked:
-                checked_currencies.append(self.currencyListWidget.item(index))
-        currencylist = [x.text() for x in checked_currencies]
+        currencylist = self.get_checked_currencies()
         currencies = json.dumps(currencylist).replace("[","(").replace("]",")").replace('"',"'")
         #print(namelist,currencylist)
         self.db.open()
@@ -245,11 +282,7 @@ class MyApp(QMainWindow, ui_design.Ui_MainWindow):
         endTime = self.endDateTimeEditor.dateTime()
         startTime.setOffsetFromUtc(0)
         endTime.setOffsetFromUtc(0)
-        checked_chans = []
-        for index in range(self.channelListWidget.count()):
-            if self.channelListWidget.item(index).checkState() == Qt.Checked:
-                checked_chans.append(self.channelListWidget.item(index))
-        namelist = [x.text() for x in checked_chans]
+        namelist = self.get_checked_chans()
         names = self.namelist_string(namelist)
         self.db.open()
         query = QSqlQuery()
@@ -274,11 +307,7 @@ class MyApp(QMainWindow, ui_design.Ui_MainWindow):
     
     def get_superchats(self):
         video_id = self.superchat_menu.currentData()
-        checked_currencies = []
-        for index in range(self.currencyListWidget.count()):
-            if self.currencyListWidget.item(index).checkState() == Qt.Checked:
-                checked_currencies.append(self.currencyListWidget.item(index))
-        currencylist = [x.text() for x in checked_currencies]
+        currencylist = self.get_checked_currencies()
         currencies = json.dumps(currencylist).replace("[","(").replace("]",")").replace('"',"'")
         self.db.open()
         customquery = "select (select name from chan_names where id = user_id and time_discovered <= time_sent order by time_discovered desc limit 1) as username, '#' || lpad(to_hex(messages.color-4278190080),6,'0') as sc_color, value, currency, message_txt, time_sent from messages inner join channel c on user_id = c.id where video_id = '"+video_id+"' and currency in " + currencies + " order by time_sent"
@@ -304,6 +333,69 @@ class MyApp(QMainWindow, ui_design.Ui_MainWindow):
         tablehead.resizeSection(0,200)
         #tablehead.resizeSections(QHeaderView.Stretch)
         self.superchat_view.resizeRowsToContents()
+        
+    def get_checked_currencies(self):
+        checked_currencies = []
+        for index in range(self.currencyListWidget.count()):
+            if self.currencyListWidget.item(index).checkState() == Qt.Checked:
+                checked_currencies.append(self.currencyListWidget.item(index))
+        currencylist = [x.text() for x in checked_currencies]
+        currencyset = set(currencylist)
+        for region in self.curr.keys():
+            if region in currencyset:
+                #currencyset.remove(region)
+                for isocurrency in self.curr[region]:
+                    currencyset.add(isocurrency)
+            
+        return list(currencyset)
+    
+    def get_checked_chans(self):
+        checked_chans = []
+        for index in range(self.channelListWidget.count()):
+            if self.channelListWidget.item(index).checkState() == Qt.Checked:
+                checked_chans.append(self.channelListWidget.item(index))
+        chanlist = [x.text() for x in checked_chans]
+        return chanlist
+        
+    def get_supa_time(self,startTime,endTime):
+        time_dict = {}
+        coord_dict = {}
+        area_sums = {}
+        #for area in self.curr.keys():
+        #    area_sums[area] = [0 for i in range(0,24)]
+        self.db.open()
+        currencylist = self.get_checked_currencies()
+        namelist = self.get_checked_chans()
+        names = self.namelist_string(namelist)
+        currencies = json.dumps(currencylist).replace("[","(").replace("]",")").replace('"',"'")
+        query = QSqlQuery()
+        query.prepare("select extract(hour from time_sent AT TIME ZONE 'UTC') as hour, count(distinct user_id) as users, currency, count(*) as donations from messages inner join video v on v.video_id = messages.video_id inner join channel c on c.id = v.channel_id WHERE c.name IN "+names+" and currency in " + currencies + " and time_sent >= :start and time_sent < :end group by currency, extract(hour from time_sent AT TIME ZONE 'UTC') order by hour")
+        query.bindValue(":start",startTime)
+        query.bindValue(":end",endTime)
+        query.exec_()
+        while query.next():
+            currency = query.value(2).strip()
+            usercount = query.value(3)
+            hournr = int(query.value(0))
+            if currency not in time_dict.keys():
+                time_dict[currency] = []
+            time_dict[currency].append((time(hour = hournr, tzinfo = timezone.utc),usercount))
+            if currency not in coord_dict.keys():
+                coord_dict[currency] = {"time":[],
+                                              "users":[]}
+            #coord_dict[query.value(2)]["time"].append(time(hour = int(query.value(0)), tzinfo = timezone.utc))
+            coord_dict[currency]["time"].append(hournr)
+            coord_dict[currency]["users"].append(usercount)
+            for area in self.curr.keys():
+                if currency in self.curr[area]:
+                    if area not in area_sums.keys():
+                        area_sums[area] = [0 for i in range(0,24)]
+                        #print("empty",area,area_sums[area])
+                    area_sums[area][hournr] += usercount
+                               
+        self.db.close()
+        return time_dict, coord_dict, area_sums
+        
         
 class MySqlModel(QSqlQueryModel):
     def data(self, index, role):
