cc213252122d45600b53a2885eba8579169da915
KaitoCross <christian_hund@hotmail.de>
2021-10-01 23:06:13 +0200

    smol fix

--- async_record_running_livestream_superchats.py
+++ async_record_running_livestream_superchats.py
@@ -95,7 +95,7 @@ class SuperchatArchiver:
             fh.setLevel(logging.DEBUG)
             ch = logging.StreamHandler()
             ch.setLevel(logging.INFO)
-            dbg_formatter = config.MyFormatter()
+            dbg_formatter = config.mylogger.MyFormatter()
             formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
             fh.setFormatter(dbg_formatter)
             ch.setFormatter(formatter)
